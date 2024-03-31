2fcb836df59d9ceb10cffc551f7c9c5a14fb3945
Suraj-Razor <14635@coderacademy.edu.au>
2024-03-31 11:02:27 +1100

    Set up files

---
+++ assignment.js
@@ -0,0 +1,7 @@
+let country = "Nepal";
+let continent = "Asia";
+let popoulation = 25;
+
+console.log(country);
+console.log(continent);
+console.log(popoulation);
---
+++ script.js
@@ -0,0 +1,87 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jona_M = 3;
+let first_Name = 23;
+let Person = "Jonas";
+let PI = 3.1415;
+
+console.log(PI);
+
+let myFirstJob = "Programmer";
+let myCurrentRole = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// // console.log(typeof 123.45 + 1);
+// // console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "dsd");
+
+// console.log(typeof javascriptIsFun);
+// javascriptIsFun = "Yes!";
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;
+
+// age = 31;
+
+// // const birthyear = 1992;
+
+// // birthyear = 1551;
+
+// // const job = 50;
+
+// var job = "programmer";
+// job = "teacher";
+// // console.log(job);s
+
+// const now = 2024;
+// const ageRay = now - 1991;
+// const ageSony = now - 1996;
+
+// console.log(ageRay, ageSony);
+// console.log(ageRay * 2, ageSony / 10, 2 ** 3);
+
+// const firstName = "Ray";
+// const lastName = "Shrestha";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 15;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x);
+
+// // comparision operator
+// console.log(ageRay > ageSony);
+// console.log(ageSony >= 18);
+
+// const isFullAge = ageSony > 18;
+// console.log(isFullAge);
+
+const now = 2024;
+const ageRay = now - 1991;
+const ageSony = now - 1996;
+
+console.log(now - 1991 > now - 2018);
